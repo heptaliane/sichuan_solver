1e44196746dfc9f5ded4888352b001ae61712d14
heptaliane <heptaliane@gmail.com>
2022-01-03 22:11:44 +0900

    Add solver

--- wasm/src/solver.rs
+++ wasm/src/solver.rs
@@ -1,7 +1,8 @@
-use super::components::{Coord, Nodes, Tile, TileMap};
+use super::components::{Coord, Nodes, SichuanError, Tile, TileMap};
 use super::connect::find_connection;
 use std::cmp::PartialEq;
 use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
 
 #[derive(Eq, PartialEq, Debug, Clone, Hash)]
 struct ConnectionInfo {
@@ -169,6 +170,166 @@ fn collect_completed_connections(map: &TileMap) -> Vec<ConnectionInfo> {
     }
 
     completed
+}
+
+#[derive(Debug)]
+struct SichuanSolver {
+    current: TileMap,
+    index_log: Vec<usize>,
+    map_log: Vec<TileMap>,
+    assumed_connections: Vec<ConnectionInfo>,
+    available_connections: Vec<Vec<ConnectionInfo>>,
+    confirmed_connections: Vec<Vec<ConnectionInfo>>,
+}
+
+#[derive(Debug, PartialEq)]
+enum SichuanSolverRollbackReason {
+    AssumeIndexOutOfRange,
+    LogicalInterpolationFailed,
+    ValidationFailed,
+}
+
+impl SichuanSolver {
+    pub fn new(map: &TileMap) -> Self {
+        Self {
+            current: map.clone(),
+            map_log: Vec::new(),
+            index_log: Vec::new(),
+            assumed_connections: Vec::new(),
+            available_connections: Vec::new(),
+            confirmed_connections: Vec::new(),
+        }
+    }
+
+    fn apply_connection_collector(
+        &mut self,
+        collector: &dyn Fn(&TileMap) -> Vec<ConnectionInfo>,
+    ) -> Vec<ConnectionInfo> {
+        let conn = collector(&self.current);
+        self.current = remove_connected_tiles(&self.current, &conn);
+        conn
+    }
+
+    fn logically_interpolate_connection(&mut self) -> Option<SichuanSolverRollbackReason> {
+        // TODO Return SichuanSolverRollbackReason::LogicalInterpolationFailed if collector fails
+        let mut connections: Vec<ConnectionInfo> = Vec::new();
+
+        loop {
+            let mut conn: Vec<ConnectionInfo> = Vec::new();
+            conn.extend(self.apply_connection_collector(&collect_completed_connections));
+
+            if conn.len() == 0 {
+                break;
+            }
+
+            connections.extend(conn);
+        }
+
+        self.confirmed_connections.push(connections);
+        self.map_log.push(self.current.clone());
+        None
+    }
+
+    fn set_available_connections(&mut self) {
+        let maplut = create_tile_lookup_table(&self.current);
+        let connlut = create_tile_connection_lookup_table(&self.current, &maplut);
+        let mut connnections: Vec<ConnectionInfo> = connlut.into_values().flatten().collect();
+        connnections.sort_by(|conn1, conn2| match (conn1.pair, conn2.pair) {
+            ([[x1, _], _], [[x2, _], _]) if x1 != x2 => x1.cmp(&x2),
+            ([[_, y1], _], [[_, y2], _]) if y1 != y2 => y1.cmp(&y2),
+            ([_, [x1, _]], [_, [x2, _]]) if x1 != x2 => x1.cmp(&x2),
+            ([_, [_, y1]], [_, [_, y2]]) => y1.cmp(&y2),
+        });
+        self.available_connections.push(connnections);
+    }
+
+    fn should_resume_index(&self) -> bool {
+        self.map_log.len() == self.index_log.len()
+    }
+
+    fn assume_connection(&mut self) -> Option<SichuanSolverRollbackReason> {
+        // Rollbacked
+        if self.should_resume_index() {
+            *self.index_log.last_mut().unwrap() += 1;
+        } else {
+            self.index_log.push(0);
+        }
+
+        let connection = self
+            .available_connections
+            .last()
+            .unwrap()
+            .get(*self.index_log.last().unwrap());
+
+        if let Some(conn) = connection {
+            self.assumed_connections.push(conn.clone());
+            self.current = remove_connected_tiles(&self.current, &vec![conn.clone()]);
+            return None;
+        }
+
+        Some(SichuanSolverRollbackReason::AssumeIndexOutOfRange)
+    }
+
+    fn is_completed(&self) -> bool {
+        Vec::from_iter(self.current.iter().flatten()).len() == 0
+    }
+
+    fn validate_current_map(&self) -> Option<SichuanSolverRollbackReason> {
+        if self.available_connections.last().unwrap().len() == 0 {
+            return Some(SichuanSolverRollbackReason::ValidationFailed);
+        }
+        None
+    }
+
+    fn rollback(&mut self, reason: SichuanSolverRollbackReason) -> Option<SichuanError> {
+        match reason {
+            SichuanSolverRollbackReason::AssumeIndexOutOfRange => match self.map_log.pop() {
+                Some(_) => {
+                    self.current = self.map_log.last().unwrap().clone();
+                    self.index_log.pop();
+                    self.assumed_connections.pop();
+                    self.available_connections.pop();
+                    self.confirmed_connections.pop();
+                }
+                None => return Some(SichuanError::NoAnswerFound),
+            },
+            SichuanSolverRollbackReason::LogicalInterpolationFailed => {
+                self.current = self.map_log.last().unwrap().clone();
+            }
+            SichuanSolverRollbackReason::ValidationFailed => match self.map_log.pop() {
+                Some(_) => {
+                    self.current = self.map_log.last().unwrap().clone();
+                    self.assumed_connections.pop();
+                    self.available_connections.pop();
+                    self.confirmed_connections.pop();
+                }
+                _ => return Some(SichuanError::NoAnswerFound),
+            },
+        }
+
+        None
+    }
+
+    fn next(&mut self) -> Option<Result<(), SichuanSolverRollbackReason>> {
+        if let Some(err) = self.assume_connection() {
+            return Some(Err(err));
+        }
+
+        if let Some(err) = self.logically_interpolate_connection() {
+            return Some(Err(err));
+        }
+
+        if self.is_completed() {
+            return Some(Ok(()));
+        }
+
+        self.set_available_connections();
+        if let Some(err) = self.validate_current_map() {
+            return Some(Err(err));
+        }
+
+        None
+    }
 }
 
 #[test]
@@ -518,4 +679,387 @@ fn test_collect_completed_connections() {
     let result = collect_completed_connections(&map);
     let actual: HashSet<&ConnectionInfo> = result.iter().collect();
     assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_sichuan_solver_logically_interpolate_connections() {
+    use ndarray::arr2;
+    let map: TileMap = arr2(&[
+        [Some(0), Some(0), Some(0), Some(0)],
+        [Some(1), Some(1), Some(2), Some(3)],
+        [Some(2), Some(1), Some(1), Some(4)],
+        [Some(4), Some(3), Some(4), Some(4)],
+    ]);
+    /*
+     * 0 0 0 0
+     * 1 1 2 3
+     * 2 1 1 4
+     * 4 3 4 4
+     */
+
+    let mut solver = SichuanSolver::new(&map);
+    solver.logically_interpolate_connection();
+
+    let expected1: HashSet<&ConnectionInfo> = [
+        ConnectionInfo {
+            pair: [[0, 0], [0, 1]],
+            tile: 0,
+            nodes: [Some([0, 0]), Some([0, 1]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[0, 2], [0, 3]],
+            tile: 0,
+            nodes: [Some([0, 2]), Some([0, 3]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[1, 0], [1, 1]],
+            tile: 1,
+            nodes: [Some([1, 0]), Some([1, 1]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[2, 1], [2, 2]],
+            tile: 1,
+            nodes: [Some([2, 1]), Some([2, 2]), None, None],
+        },
+    ]
+    .iter()
+    .collect();
+    let expected2: HashSet<&ConnectionInfo> = [
+        ConnectionInfo {
+            pair: [[1, 2], [2, 0]],
+            tile: 2,
+            nodes: [Some([1, 2]), Some([2, 2]), Some([2, 0]), None],
+        },
+        ConnectionInfo {
+            pair: [[1, 3], [3, 1]],
+            tile: 3,
+            nodes: [Some([3, 1]), Some([0, 1]), Some([0, 3]), Some([1, 3])],
+        },
+    ]
+    .iter()
+    .collect();
+    let expected3: HashSet<&ConnectionInfo> = [
+        ConnectionInfo {
+            pair: [[2, 3], [3, 0]],
+            tile: 4,
+            nodes: [Some([2, 3]), Some([2, 0]), Some([3, 0]), None],
+        },
+        ConnectionInfo {
+            pair: [[3, 2], [3, 3]],
+            tile: 4,
+            nodes: [Some([3, 2]), Some([3, 3]), None, None],
+        },
+    ]
+    .iter()
+    .collect();
+
+    let mut connections = solver.confirmed_connections[0].clone();
+    let conn3 = connections.split_off(6);
+    let actual3: HashSet<&ConnectionInfo> = conn3.iter().collect();
+    let conn2 = connections.split_off(4);
+    let actual2: HashSet<&ConnectionInfo> = conn2.iter().collect();
+    let actual1: HashSet<&ConnectionInfo> = connections.iter().collect();
+
+    assert_eq!(actual1, expected1);
+    assert_eq!(actual2, expected2);
+    assert_eq!(actual3, expected3);
+}
+
+#[test]
+fn test_sichuan_solver_is_completed() {
+    use ndarray::arr2;
+    let map: TileMap = arr2(&[
+        [Some(0), Some(0), Some(0), Some(0)],
+        [Some(1), Some(1), Some(2), Some(3)],
+        [Some(2), Some(1), Some(1), Some(4)],
+        [Some(4), Some(3), Some(4), Some(4)],
+    ]);
+    /*
+     * 0 0 0 0
+     * 1 1 2 3
+     * 2 1 1 4
+     * 4 3 4 4
+     */
+
+    let mut solver = SichuanSolver::new(&map);
+    assert_eq!(solver.is_completed(), false);
+
+    solver.logically_interpolate_connection();
+    assert_eq!(solver.is_completed(), true);
+
+    let map2: TileMap = arr2(&[[Some(0), Some(1)], [Some(1), Some(0)]]);
+    /*
+     * 0 1
+     * 1 0
+     */
+    let mut solver2 = SichuanSolver::new(&map2);
+    assert_eq!(solver2.is_completed(), false);
+
+    solver.logically_interpolate_connection();
+    assert_eq!(solver2.is_completed(), false);
+}
+
+#[test]
+fn test_sichuan_solver_set_available_connections() {
+    use ndarray::arr2;
+    let map: TileMap = arr2(&[
+        [Some(0), Some(0), Some(1), Some(0)],
+        [Some(0), Some(1), Some(1), Some(2)],
+        [Some(1), Some(2), Some(2), Some(3)],
+        [Some(3), Some(2), Some(3), Some(3)],
+    ]);
+    /*
+     * 0 0 1 0
+     * 0 1 1 2
+     * 1 2 2 3
+     * 3 2 3 3
+     */
+    let mut solver = SichuanSolver::new(&map);
+    solver.logically_interpolate_connection();
+    solver.set_available_connections();
+
+    let expected: HashSet<&ConnectionInfo> = [
+        ConnectionInfo {
+            pair: [[0, 0], [0, 1]],
+            tile: 0,
+            nodes: [Some([0, 0]), Some([0, 1]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[0, 0], [1, 0]],
+            tile: 0,
+            nodes: [Some([0, 0]), Some([1, 0]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[0, 2], [1, 2]],
+            tile: 1,
+            nodes: [Some([0, 2]), Some([1, 2]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[1, 1], [1, 2]],
+            tile: 1,
+            nodes: [Some([1, 1]), Some([1, 2]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[2, 1], [2, 2]],
+            tile: 2,
+            nodes: [Some([2, 1]), Some([2, 2]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[2, 1], [3, 1]],
+            tile: 2,
+            nodes: [Some([2, 1]), Some([3, 1]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[2, 3], [3, 3]],
+            tile: 3,
+            nodes: [Some([2, 3]), Some([3, 3]), None, None],
+        },
+        ConnectionInfo {
+            pair: [[3, 2], [3, 3]],
+            tile: 3,
+            nodes: [Some([3, 2]), Some([3, 3]), None, None],
+        },
+    ]
+    .iter()
+    .collect();
+
+    let connections = solver.available_connections.last().clone().unwrap();
+    let actual: HashSet<&ConnectionInfo> = connections.iter().collect();
+    assert_eq!(actual, expected);
+
+    assert_eq!(solver.current, map);
+    assert_eq!(solver.map_log.len(), 1);
+    assert_eq!(solver.confirmed_connections, vec![vec![]]);
+    assert_eq!(solver.assumed_connections, vec![]);
+    assert_eq!(solver.index_log, vec![]);
+}
+
+fn test_sichuan_solver_assume_connection() {
+    use ndarray::arr2;
+    let map: TileMap = arr2(&[
+        [Some(0), Some(0), Some(1), Some(0)],
+        [Some(0), Some(1), Some(1), Some(2)],
+        [Some(1), Some(2), Some(2), Some(3)],
+        [Some(3), Some(2), Some(3), Some(3)],
+    ]);
+    /*
+     * 0 0 1 0
+     * 0 1 1 2
+     * 1 2 2 3
+     * 3 2 3 3
+     */
+    let mut solver = SichuanSolver::new(&map);
+    solver.logically_interpolate_connection();
+    solver.set_available_connections();
+
+    assert_eq!(solver.assumed_connections, vec![]);
+    solver.assume_connection();
+    assert_eq!(
+        solver.assumed_connections,
+        vec![ConnectionInfo {
+            pair: [[0, 0], [0, 1]],
+            tile: 0,
+            nodes: [Some([0, 0]), Some([0, 1]), None, None],
+        },]
+    );
+    assert_eq!(solver.index_log, vec![0]);
+    assert_ne!(solver.current, map);
+    assert_eq!(solver.map_log.len(), 1);
+    assert_eq!(solver.confirmed_connections, vec![vec![]]);
+
+    let newmap: TileMap = arr2(&[
+        [None, None, Some(1), Some(0)],
+        [Some(0), Some(1), Some(1), Some(2)],
+        [Some(1), Some(2), Some(2), Some(3)],
+        [Some(3), Some(2), Some(3), Some(3)],
+    ]);
+    assert_eq!(solver.current, newmap);
+
+    let map2: TileMap = arr2(&[[Some(0), Some(1)], [Some(1), Some(0)]]);
+    /*
+     * 0 1
+     * 1 0
+     */
+    let mut solver2 = SichuanSolver::new(&map2);
+    solver2.logically_interpolate_connection();
+    solver2.set_available_connections();
+    assert_eq!(
+        solver2.assume_connection(),
+        Some(SichuanSolverRollbackReason::AssumeIndexOutOfRange)
+    );
+}
+
+#[test]
+fn test_sichuan_solver_validate_current_map() {
+    use ndarray::arr2;
+    let map: TileMap = arr2(&[
+        [Some(0), Some(0), Some(1), Some(0)],
+        [Some(0), Some(1), Some(1), Some(2)],
+        [Some(3), Some(2), Some(2), Some(3)],
+        [Some(3), Some(2), Some(3), Some(1)],
+    ]);
+    /*
+     * 0 0 1 0
+     * 0 1 1 2
+     * 3 2 2 3
+     * 3 2 3 1
+     */
+    let mut solver = SichuanSolver::new(&map);
+    solver.logically_interpolate_connection();
+    solver.set_available_connections();
+    solver.assume_connection();
+    solver.logically_interpolate_connection();
+    solver.set_available_connections();
+    assert_eq!(solver.is_completed(), false);
+    assert_eq!(solver.validate_current_map(), None);
+
+    let map2: TileMap = arr2(&[
+        [None, None, None, None],
+        [Some(0), Some(0), Some(0), Some(1)],
+        [None, None, Some(1), Some(0)],
+    ]);
+    /*
+     * x x x x
+     * 0 0 0 1
+     * x x 1 0
+     */
+    let mut solver2 = SichuanSolver::new(&map2);
+    solver2.logically_interpolate_connection();
+    solver2.set_available_connections();
+    solver2.assume_connection();
+    solver2.logically_interpolate_connection();
+    solver2.set_available_connections();
+    assert_eq!(solver2.is_completed(), false);
+    assert_eq!(
+        solver2.validate_current_map(),
+        Some(SichuanSolverRollbackReason::ValidationFailed)
+    );
+}
+
+#[test]
+fn test_sichuan_solver_rollback() {
+    use ndarray::arr2;
+    let map: TileMap = arr2(&[
+        [Some(0), Some(0), Some(0), Some(1)],
+        [None, None, Some(1), Some(0)],
+    ]);
+    /*
+     * 0 0 0 1
+     * x x 1 0
+     */
+    let mut solver = SichuanSolver::new(&map);
+    solver.logically_interpolate_connection();
+    solver.set_available_connections();
+    solver.assume_connection();
+    solver.logically_interpolate_connection();
+    solver.set_available_connections();
+    assert_eq!(solver.is_completed(), false);
+
+    let reason = solver.validate_current_map();
+    solver.rollback(reason.unwrap());
+    assert_eq!(solver.map_log.len(), 1);
+    assert_eq!(solver.index_log.len(), 1);
+    assert_eq!(solver.confirmed_connections.len(), 1);
+    assert_eq!(solver.available_connections.len(), 1);
+    assert_eq!(solver.assumed_connections.len(), 0);
+    assert_eq!(solver.should_resume_index(), true);
+    assert_eq!(&solver.current, solver.map_log.last().unwrap());
+
+    solver.assume_connection();
+    assert_eq!(solver.assumed_connections.len(), 1);
+    assert_eq!(solver.index_log, vec![1]);
+    solver.logically_interpolate_connection();
+    solver.set_available_connections();
+    assert!(solver.is_completed());
+
+    let map2: TileMap = arr2(&[
+        [Some(0), Some(1), None, Some(0)],
+        [Some(1), Some(1), Some(2), Some(3)],
+        [Some(2), Some(3), Some(0), None],
+        [None, Some(0), Some(1), None],
+    ]);
+    /*
+     * 0 1 x 0
+     * 1 1 2 3
+     * 2 3 0 x
+     * x 0 1 x
+     */
+    let mut solver2 = SichuanSolver::new(&map2);
+    solver2.logically_interpolate_connection();
+    solver2.set_available_connections();
+    solver2.assume_connection();
+    solver2.logically_interpolate_connection();
+    solver2.set_available_connections();
+    assert_eq!(solver2.is_completed(), false);
+    assert_eq!(solver2.validate_current_map(), None);
+
+    assert_eq!(solver2.should_resume_index(), false);
+    solver2.assume_connection();
+    solver2.logically_interpolate_connection();
+    solver2.set_available_connections();
+
+    let reason2 = solver2.validate_current_map();
+    solver2.rollback(reason2.unwrap());
+    assert_eq!(solver2.map_log.len(), 2);
+    assert_eq!(solver2.index_log.len(), 2);
+    assert_eq!(solver2.confirmed_connections.len(), 2);
+    assert_eq!(solver2.available_connections.len(), 2);
+    assert_eq!(solver2.assumed_connections.len(), 1);
+    assert_eq!(solver2.should_resume_index(), true);
+    assert_eq!(&solver2.current, solver2.map_log.last().unwrap());
+
+    let reason3 = solver2.assume_connection();
+    solver2.rollback(reason3.unwrap());
+    assert_eq!(solver2.map_log.len(), 1);
+    assert_eq!(solver2.index_log.len(), 1);
+    assert_eq!(solver2.confirmed_connections.len(), 1);
+    assert_eq!(solver2.available_connections.len(), 1);
+    assert_eq!(solver2.assumed_connections.len(), 0);
+    assert_eq!(solver2.should_resume_index(), true);
+    assert_eq!(&solver2.current, solver2.map_log.last().unwrap());
+
+    solver2.assume_connection();
+    solver2.logically_interpolate_connection();
+    solver2.set_available_connections();
+    assert_eq!(solver2.is_completed(), true);
 }
