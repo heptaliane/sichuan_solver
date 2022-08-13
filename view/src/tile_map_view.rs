43cbf6aca6922e8e8f531244221e700f0478a562
heptaliane <heptaliane@gmail.com>
2022-08-13 23:03:39 +0900

    refactored

--- view/src/tile_map_view.rs
+++ view/src/tile_map_view.rs
@@ -6,7 +6,7 @@ use yew::{NodeRef, Properties};
 
 use super::icon::tile::MahjongTileImage;
 
-const DEFAULT_TILE_WIDTH: usize = 100;
+const DEFAULT_TILE_WIDTH: usize = 80;
 const DEFAULT_TILE_HEIGHT: usize = 100;
 const DEFAULT_MAP_ROWS: usize = 5;
 const DEFAULT_MAP_COLS: usize = 5;
@@ -14,6 +14,7 @@ const GRID_LINE_WIDTH: usize = 1;
 const GRID_STYLE: &str = "lightgray";
 const ACTIVE_LINE_STYLE: &str = "red";
 const ACTIVE_LINE_WIDTH: usize = 3;
+const ACTIVE_BG_COLOR: &str = "lightyellow";
 
 pub enum TileMapViewMsg {
     TileClicked([i32; 2]),
@@ -24,7 +25,7 @@ pub struct TileMapViewModel {
     canvas: NodeRef,
     height: usize,
     width: usize,
-    active: Option<[i32; 2]>,
+    active: Option<[usize; 2]>,
 }
 
 #[derive(Properties, PartialEq)]
@@ -53,8 +54,8 @@ impl Component for TileMapViewModel {
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             TileMapViewMsg::TileClicked([x, y]) => {
-                let i: i32 = (x as f64 / self.width as f64).floor() as i32;
-                let j: i32 = (y as f64 / self.height as f64).floor() as i32;
+                let i: usize = self.xpos(x);
+                let j: usize = self.ypos(y);
                 match self.active {
                     Some([current_i, current_j]) if current_i == i && current_j == j => {
                         self.active = None
@@ -73,8 +74,8 @@ impl Component for TileMapViewModel {
         html! {
             <canvas
                 ref={self.canvas.clone()}
-                height={(DEFAULT_TILE_HEIGHT * ctx.props().rows + GRID_LINE_WIDTH * 2).to_string()}
-                width={(DEFAULT_TILE_WIDTH * ctx.props().cols + GRID_LINE_WIDTH * 2).to_string()}
+                height={self.map_height(ctx).to_string()}
+                width={self.map_width(ctx).to_string()}
                 onclick={onclick}
             >
             </canvas>
@@ -85,7 +86,7 @@ impl Component for TileMapViewModel {
         self.clear_canvas(ctx);
         self.draw_grids(ctx);
         match self.active {
-            Some([x, y]) => self.draw_active_tile(x, y),
+            Some([x, y]) => self.draw_active_tile(x as usize, y as usize),
             _ => (),
         }
         self.draw_tile_images(ctx);
@@ -104,72 +105,80 @@ impl TileMapViewModel {
             .unwrap()
     }
 
+    fn map_height(&self, ctx: &Context<Self>) -> f64 {
+        (DEFAULT_TILE_HEIGHT * ctx.props().cols + GRID_LINE_WIDTH * 2) as f64
+    }
+
+    fn map_width(&self, ctx: &Context<Self>) -> f64 {
+        (DEFAULT_TILE_WIDTH * ctx.props().rows + GRID_LINE_WIDTH * 2) as f64
+    }
+
+    fn tile_top(&self, i: usize) -> f64 {
+        (self.height * i + GRID_LINE_WIDTH) as f64
+    }
+
+    fn tile_left(&self, i: usize) -> f64 {
+        (self.width * i + GRID_LINE_WIDTH) as f64
+    }
+
+    fn xpos(&self, xpixel: i32) -> usize {
+        (xpixel as f64 / self.width as f64).floor() as usize
+    }
+
+    fn ypos(&self, ypixel: i32) -> usize {
+        (ypixel as f64 / self.height as f64).floor() as usize
+    }
+
     fn clear_canvas(&self, ctx: &Context<Self>) {
         let context = self.canvas_context();
-        let (rows, cols) = (ctx.props().rows, ctx.props().cols);
-        let (w, h) = (self.width, self.height);
 
-        context.clear_rect(
-            0.,
-            0.,
-            (w * cols + GRID_LINE_WIDTH * 2) as f64,
-            (h * rows + GRID_LINE_WIDTH * 2) as f64,
-        );
+        context.clear_rect(0., 0., self.map_width(ctx), self.map_height(ctx));
     }
 
     fn draw_grids(&self, ctx: &Context<Self>) {
         let (rows, cols) = (ctx.props().rows, ctx.props().cols);
-        let (w, h) = (self.width, self.height);
         let context = self.canvas_context();
 
         context.set_stroke_style(&JsValue::from_str(GRID_STYLE));
         context.set_line_width(GRID_LINE_WIDTH as f64);
         for i in 0..=cols {
-            context.move_to((w * i + GRID_LINE_WIDTH) as f64, 0.);
-            context.line_to(
-                (w * i + GRID_LINE_WIDTH) as f64,
-                (h * rows + GRID_LINE_WIDTH) as f64,
-            );
+            context.move_to(self.tile_left(i), 0.);
+            context.line_to(self.tile_left(i), self.tile_top(rows));
             context.stroke();
         }
         for i in 0..=rows {
-            context.move_to(0., (h * i + GRID_LINE_WIDTH) as f64);
-            context.line_to(
-                (w * cols + GRID_LINE_WIDTH) as f64,
-                (h * i + GRID_LINE_WIDTH) as f64,
-            );
+            context.move_to(0., self.tile_top(i));
+            context.line_to(self.tile_left(cols), self.tile_top(i));
             context.stroke();
         }
     }
 
-    fn draw_active_tile(&self, x: i32, y: i32) {
-        let (w, h) = (self.width, self.height);
+    fn draw_active_tile(&self, x: usize, y: usize) {
+        let (w, h) = (self.width as f64, self.height as f64);
         let context = self.canvas_context();
+
+        context.set_fill_style(&JsValue::from_str(ACTIVE_BG_COLOR));
+        context.fill_rect(self.tile_left(x), self.tile_top(y), w, h);
 
         context.set_stroke_style(&JsValue::from_str(ACTIVE_LINE_STYLE));
         context.set_line_width(ACTIVE_LINE_WIDTH as f64);
-        context.stroke_rect(
-            (w as i32 * x) as f64,
-            (h as i32 * y) as f64,
-            w as f64,
-            h as f64,
-        );
+        context.stroke_rect(self.tile_left(x), self.tile_top(y), w, h);
     }
 
     fn draw_tile_images(&self, ctx: &Context<Self>) {
-        let (w, h) = (self.width, self.height);
+        let (w, h) = (self.width as f64, self.height as f64);
         let context = self.canvas_context();
 
         for (&(i, j), &idx) in &ctx.props().tile_map {
             let result = context.draw_image_with_html_image_element_and_dw_and_dh(
                 self.image_data.get_ref(idx),
-                (w as f64) * (i as f64),
-                (h as f64) * (j as f64),
-                w as f64,
-                h as f64,
+                self.tile_left(i),
+                self.tile_top(j),
+                w,
+                h,
             );
             match result {
-                Err(e) => log::info!("{}", e.as_string().unwrap_or("None".to_string())),
+                Err(_) => log::info!("Failed to load image."),
                 _ => (),
             }
         }
