9f845923a7925c789bc16513eacb9bfdc635ae88
heptaliane <heptaliane@gmail.com>
2023-04-27 22:23:27 +0900

    Create coord pair constructor

--- src/solver/lut.rs
+++ src/solver/lut.rs
@@ -2,6 +2,7 @@ use super::super::components::{Coord, Tile, TileMap};
 use std::collections::{HashMap, HashSet};
 
 pub type CoordCollection = HashMap<Tile, HashSet<Coord>>;
+pub type CoordPairCollection = HashMap<Tile, HashSet<[Coord; 2]>>;
 
 pub fn tile_map_to_coord_collection(map: &TileMap) -> CoordCollection {
     let mut collection: CoordCollection = HashMap::new();
@@ -18,6 +19,17 @@ pub fn tile_map_to_coord_collection(map: &TileMap) -> CoordCollection {
     collection
 }
 
+fn create_coord_pair(coords: &HashSet<Coord>) -> HashSet<[Coord; 2]> {
+    let ordered_coords: Vec<Coord> = coords.clone().into_iter().collect();
+    let mut pairs: HashSet<[Coord; 2]> = HashSet::new();
+    for i in 0..ordered_coords.len() {
+        for j in (i + 1)..ordered_coords.len() {
+            pairs.insert([ordered_coords[i], ordered_coords[j]]);
+        }
+    }
+    pairs
+}
+
 #[test]
 fn test_tile_map_to_coord_collection() {
     let map: TileMap = HashMap::from([([0, 0], 0), ([1, 1], 1), ([2, 3], 2), ([1, 0], 1)]);
@@ -31,4 +43,10 @@ fn test_tile_map_to_coord_collection() {
     assert_eq!(actual.get(&0), expected.get(&0));
     assert_eq!(actual.get(&1), expected.get(&1));
     assert_eq!(actual.get(&2), expected.get(&2));
+}
+
+#[test]
+fn test_create_coord_pair() {
+    let coords: HashSet<Coord> = HashSet::from([[0, 0], [0, 1], [1, 0], [1, 1]]);
+    assert_eq!(create_coord_pair(&coords).len(), 6);
 }
