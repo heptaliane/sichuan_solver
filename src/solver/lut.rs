dc0571abc5b4c03de6c99ecbd87062642fbd6f0f
heptaliane <heptaliane@gmail.com>
2023-04-28 21:28:20 +0900

    Make lookup table sortable

--- src/solver/lut.rs
+++ src/solver/lut.rs
@@ -1,31 +1,39 @@
 use super::super::components::{Coord, Tile, TileMap};
 use std::collections::{HashMap, HashSet};
 
-pub type CoordCollection = HashMap<Tile, HashSet<Coord>>;
-pub type CoordPairCollection = HashMap<Tile, HashSet<[Coord; 2]>>;
+pub type CoordCollection = HashMap<Tile, Vec<Coord>>;
+pub type CoordPairCollection = HashMap<Tile, Vec<[Coord; 2]>>;
 
 pub fn tile_map_to_coord_collection(map: &TileMap) -> CoordCollection {
     let mut collection: CoordCollection = HashMap::new();
     for (&coord, &tile) in map.iter() {
         match collection.get_mut(&tile) {
             Some(coords) => {
-                coords.insert(coord);
+                coords.push(coord);
             }
             _ => {
-                collection.insert(tile, HashSet::from([coord]));
+                collection.insert(tile, vec![coord]);
             }
         }
     }
     collection
 }
 
-fn create_coord_pair(coords: &HashSet<Coord>) -> HashSet<[Coord; 2]> {
+fn create_coord_pair(coords: &Vec<Coord>) -> Vec<[Coord; 2]> {
     let ordered_coords: Vec<Coord> = coords.clone().into_iter().collect();
-    let mut pairs: HashSet<[Coord; 2]> = HashSet::new();
+    let mut pairs: Vec<[Coord; 2]> = Vec::new();
     for i in 0..ordered_coords.len() {
         for j in (i + 1)..ordered_coords.len() {
-            pairs.insert([ordered_coords[i], ordered_coords[j]]);
+            pairs.push([ordered_coords[i], ordered_coords[j]]);
         }
+    }
+    pairs
+}
+
+pub fn create_coord_pair_collection(lut: &CoordCollection) -> CoordPairCollection {
+    let mut pairs: CoordPairCollection = HashMap::new();
+    for (&tile, coords) in lut.iter() {
+        pairs.insert(tile, create_coord_pair(coords));
     }
     pairs
 }
@@ -33,20 +41,67 @@ fn create_coord_pair(coords: &HashSet<Coord>) -> HashSet<[Coord; 2]> {
 #[test]
 fn test_tile_map_to_coord_collection() {
     let map: TileMap = HashMap::from([([0, 0], 0), ([1, 1], 1), ([2, 3], 2), ([1, 0], 1)]);
-    let expected: CoordCollection = HashMap::from([
-        (0, HashSet::from([[0, 0]])),
-        (1, HashSet::from([[1, 1], [1, 0]])),
-        (2, HashSet::from([[2, 3]])),
-    ]);
-    let actual = tile_map_to_coord_collection(&map);
+    let mut actual = tile_map_to_coord_collection(&map);
+    for coords in actual.values_mut() {
+        coords.sort();
+    }
 
-    assert_eq!(actual.get(&0), expected.get(&0));
-    assert_eq!(actual.get(&1), expected.get(&1));
-    assert_eq!(actual.get(&2), expected.get(&2));
+    let mut actual1: Vec<Coord> = actual[&0].clone();
+    let expected1 = vec![[0, 0]];
+    assert_eq!(actual1, expected1);
+
+    let mut actual2: Vec<Coord> = actual[&1].clone();
+    let expected2 = vec![[1, 0], [1, 1]];
+    assert_eq!(actual2, expected2);
+
+    let mut actual3: Vec<Coord> = actual[&2].clone();
+    let expected3 = vec![[2, 3]];
+    assert_eq!(actual3, expected3);
 }
 
 #[test]
 fn test_create_coord_pair() {
-    let coords: HashSet<Coord> = HashSet::from([[0, 0], [0, 1], [1, 0], [1, 1]]);
-    assert_eq!(create_coord_pair(&coords).len(), 6);
+    let coords: Vec<Coord> = vec![[0, 0], [1, 0], [0, 1], [1, 1]];
+    let mut pair_set = create_coord_pair(&coords);
+    pair_set.iter_mut().for_each(|coords| coords.sort());
+    pair_set.sort();
+    assert_eq!(
+        pair_set,
+        vec![
+            [[0, 0], [0, 1]],
+            [[0, 0], [1, 0]],
+            [[0, 0], [1, 1]],
+            [[0, 1], [1, 0]],
+            [[0, 1], [1, 1]],
+            [[1, 0], [1, 1]]
+        ]
+    );
+}
+
+#[test]
+fn test_create_coord_pair_collection() {
+    let lut: CoordCollection = HashMap::from([
+        (0, vec![[0, 0], [0, 1]]),
+        (1, vec![[1, 0], [1, 2], [1, 3], [1, 1]]),
+        (2, vec![[2, 0], [2, 1]]),
+    ]);
+    let mut actual = create_coord_pair_collection(&lut);
+    for coords in actual.values_mut() {
+        coords.iter_mut().for_each(|pair| pair.sort());
+        coords.sort();
+    }
+
+    assert_eq!(actual[&0], vec![[[0, 0], [0, 1]]]);
+    assert_eq!(
+        actual[&1],
+        vec![
+            [[1, 0], [1, 1]],
+            [[1, 0], [1, 2]],
+            [[1, 0], [1, 3]],
+            [[1, 1], [1, 2]],
+            [[1, 1], [1, 3]],
+            [[1, 2], [1, 3]]
+        ]
+    );
+    assert_eq!(actual[&2], vec![[[2, 0], [2, 1]]]);
 }
